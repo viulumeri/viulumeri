3477184859745149a6521bda093be3b1d1f52f02
Jasse <jasse.merivirta@helsinki.fi>
2025-08-05 13:18:51 +0300

    Add React Router and a route for signup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,21 @@
 import { Signup } from './components/SignUp.tsx'
+import {
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Routes,
+  useMatch,
+  useNavigate
+} from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
       <h1>Viulumeri</h1>
-      <Signup />
+
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </div>
   )
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App.tsx'
 
 const queryClient = new QueryClient()
@@ -8,7 +9,9 @@ const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </QueryClientProvider>
-  </StrictMode>,
+  </StrictMode>
 )

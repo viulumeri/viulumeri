d17233111b2fcfe00681f7cd350cb03bffb03d2f
Jasse <jasse.merivirta@helsinki.fi>
2025-08-05 13:02:38 +0300

    Refactor Signup to its own component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,60 +1,10 @@
-import { useField } from './hooks/useField'
-import { useSignUp } from './hooks/useAuth'
+import { Signup } from './components/SignUp.tsx'
 
 const App = () => {
-  const email = useField('text')
-  const password = useField('password')
-  const name = useField('text')
-
-  const signUpMutation = useSignUp({
-    onSuccess: () => {
-      console.log(`Sign up successful for ${name.value}`)
-      email.reset()
-      password.reset()
-      name.reset()
-    },
-    onError: (error) => {
-      console.error(error instanceof Error ? error.message : 'Sign up failed.')
-    }
-  })
-
-  const handleSignUp = (event: React.FormEvent) => {
-    event.preventDefault()
-    signUpMutation.mutate({
-      email: email.value,
-      password: password.value,
-      name: name.value
-    })
-  }
-
   return (
     <div>
       <h1>Viulumeri</h1>
-      <h2>Sign up</h2>
-      <form onSubmit={handleSignUp}>
-        <div>
-          email <input {...email.props} />
-        </div>
-        <div>
-          password <input {...password.props} />
-        </div>
-        <div>
-          name <input {...name.props} />
-        </div>
-        <button type="submit" disabled={signUpMutation.isPending}>
-          {signUpMutation.isPending ? 'Signing up...' : 'Sign Up'}
-        </button>
-      </form>
-      {signUpMutation.isError && (
-        <div style={{ color: 'red' }}>
-          {signUpMutation.error instanceof Error
-            ? signUpMutation.error.message
-            : 'Sign up failed'}
-        </div>
-      )}
-      {signUpMutation.isSuccess && (
-        <div style={{ color: 'green' }}>Sign up successful!</div>
-      )}
+      <Signup />
     </div>
   )
 }

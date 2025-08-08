8016079c08118355186b0d6c252aca903c361e95
Jasse <jasse.merivirta@helsinki.fi>
2025-08-08 18:33:27 +0300

    Add mongoose schemas for teachers and students

---
+++ server/src/models/student.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+const studentSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  }
+})
+
+studentSchema.set('toJSON', {
+  transform: (document: any, returnedObject: any) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+export default mongoose.model('Student', studentSchema)
---
+++ server/src/models/teacher.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+const teacherSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  }
+})
+
+teacherSchema.set('toJSON', {
+  transform: (document: any, returnedObject: any) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+export default mongoose.model('Teacher', teacherSchema)

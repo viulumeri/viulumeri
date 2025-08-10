3bd26c3510ca59da80c1598760870f9773a133a9
Jasse <jasse.merivirta@helsinki.fi>
2025-08-10 13:22:17 +0300

    Add service init to server start

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,10 +4,14 @@ import connectDB from './db'
 import { toNodeHandler } from 'better-auth/node'
 import { auth } from './utils/auth'
 import { corsOrigin } from './utils/config'
+import { musicService } from './services/music'
 const middleware = require('./utils/middleware.ts')
+import songsRouter from './controllers/songs'
 
 const app = express()
+
 connectDB()
+musicService.initialize()
 
 app.use(
   cors({
@@ -21,6 +25,7 @@ app.use(middleware.requestLogger)
 app.all('/api/auth/*splat', toNodeHandler(auth))
 
 app.use(express.json())
+app.use('/api/songs', songsRouter)
 app.get('/ping', (req, res) => {
   res.send('pong')
 })

27419be32c971fb1f38c78315c18737677fcf9d5
Iida Häkkinen <iida.hakkinen@gmail.com>
2025-08-19 00:35:30 +0300

    Add Homework types

--- shared/types.ts
+++ shared/types.ts
@@ -37,3 +37,34 @@ export interface GenerateInviteResponse {
   inviteUrl: string
   expiresIn: string
 }
+
+export interface Homework {
+  id: string
+  student: { id: string; name: string }
+  songs: string[]
+  comment: string
+  practiceCount: number
+  createdAt: string
+  // returned by GET /api/student/homework and GET /api/teacher/students/:id/homework
+}
+
+export interface CreateHomeworkBody {
+  studentId: string
+  songs?: string[]
+  comment?: string
+  // client sends in POST /api/teacher/homework
+}
+
+export interface CreateHomeworkResponse extends Homework {
+  // response to POST /api/teacher/homework)
+}
+
+export interface HomeworkListResponse {
+  homework: Homework[]
+  // response to GET /api/student/homework and and GET /api/teacher/students/:id/homework
+}
+
+export interface PracticeResponse {
+  id: string
+  practiceCount: number
+}

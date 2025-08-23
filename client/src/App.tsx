00b71a3dab39888a894197162339acb4c9411834
Iida Häkkinen <iida.hakkinen@gmail.com>
2025-08-23 16:10:08 +0300

    Add initial layout and components for student list

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,6 +11,7 @@ import { CreateHomeworkPage } from './components/CreateHomeworkPage'
 import { Link, Route, Routes, useNavigate } from 'react-router-dom'
 import { authClient, useSession } from './auth-client'
 import type { AppSessionUser } from '../../shared/types'
+import './index.css'
 
 const App = () => {
   const { data: session } = useSession()
@@ -27,59 +28,61 @@ const App = () => {
   }
 
   return (
-    <div>
-      <h1>Viulumeri</h1>
+    <div className="min-h-screen bg-gray-100 flex justify-center">
+      <div className="w-full max-w-sm min-h-screen bg-neutral-900 text-gray-100">
+        <h1>Viulumeri</h1>
 
-      <nav>
-        {!session && (
-          <>
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Uusi käyttäjä</Link>
-          </>
-        )}
+        <nav>
+          {!session && (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Uusi käyttäjä</Link>
+            </>
+          )}
 
-        {session && (
-          <>
-            <span>Tervetuloa, {session.user.email}!</span>
-            <button onClick={handleSignOut}>Logout</button>
-            <Link to="/songslist">Biisilista</Link>
-            {/* Teippi*/}
-            {(session?.user as unknown as AppSessionUser | undefined)
-              ?.userType === 'teacher' && (
-              <>
-                <Link to="/invite">Lisää uusi oppilas</Link>
-                <Link to="/teacher/students">Oppilaat</Link>
-              </>
-            )}
-            {(session?.user as unknown as AppSessionUser | undefined)
-              ?.userType === 'student' && (
-              <>
-                <Link to="/student/homework">Tehtävät</Link>
-              </>
-            )}
-          </>
-        )}
-      </nav>
+          {session && (
+            <>
+              <span>Tervetuloa, {session.user.email}!</span>
+              <button onClick={handleSignOut}>Logout</button>
+              <Link to="/songslist">Biisilista</Link>
+              {/* Teippi*/}
+              {(session?.user as unknown as AppSessionUser | undefined)
+                ?.userType === 'teacher' && (
+                <>
+                  <Link to="/invite">Lisää uusi oppilas</Link>
+                  <Link to="/teacher/students">Oppilaat</Link>
+                </>
+              )}
+              {(session?.user as unknown as AppSessionUser | undefined)
+                ?.userType === 'student' && (
+                <>
+                  <Link to="/student/homework">Tehtävät</Link>
+                </>
+              )}
+            </>
+          )}
+        </nav>
 
-      <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<h2>Etusivu placeholder</h2>} />
-        <Route path="/songslist" element={<Songslist />} />
-        <Route path="/player/:songId" element={<MusicPlayer />} />
-        <Route path="/invite" element={<InviteLink />} />
-        <Route path="/invite/:token" element={<InviteAccept />} />
-        <Route path="/teacher/students" element={<TeacherStudentsPage />} />
-        <Route
-          path="/teacher/students/:studentId/homework"
-          element={<TeacherStudentHomeworkPage />}
-        />
-        <Route
-          path="/teacher/students/:studentId/homework/create"
-          element={<CreateHomeworkPage />}
-        />
-        <Route path="/student/homework" element={<StudentHomeworkPage />} />
-      </Routes>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<h2>Etusivu placeholder</h2>} />
+          <Route path="/songslist" element={<Songslist />} />
+          <Route path="/player/:songId" element={<MusicPlayer />} />
+          <Route path="/invite" element={<InviteLink />} />
+          <Route path="/invite/:token" element={<InviteAccept />} />
+          <Route path="/teacher/students" element={<TeacherStudentsPage />} />
+          <Route
+            path="/teacher/students/:studentId/homework"
+            element={<TeacherStudentHomeworkPage />}
+          />
+          <Route
+            path="/teacher/students/:studentId/homework/create"
+            element={<CreateHomeworkPage />}
+          />
+          <Route path="/student/homework" element={<StudentHomeworkPage />} />
+        </Routes>
+      </div>
     </div>
   )
 }

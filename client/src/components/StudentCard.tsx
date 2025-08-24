0b3b81234a848d50c8e588fa471cfcdb4157b468
Iida Häkkinen <iida.hakkinen@gmail.com>
2025-08-24 19:03:27 +0300

    Add student card component

---
+++ client/src/components/StudentCard.tsx
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom'
+
+type Props = {
+  id: string
+  name: string
+}
+
+export function StudentCard({ id, name }: Props) {
+  return (
+    <Link
+      to={`/teacher/students/${id}/homework`}
+      className="relative rounded-md aspect-square text-justify-center overflow-hidden"
+    >
+      <h2 className="absolute bottom-4 left-6 text-white z-10">{name}</h2>
+      <img
+        src="https://images.metmuseum.org/CRDImages/ep/web-large/DT2145.jpg"
+        alt="kuva"
+        className="absolute inset-0 w-full h-full object-cover rounded-md z-0"
+      />
+    </Link>
+  )
+}

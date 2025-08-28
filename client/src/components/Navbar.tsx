84b7936dd4fcc198f231927ba4c24da714e29a01
Iida Häkkinen <iida.hakkinen@gmail.com>
2025-08-28 21:41:37 +0300

    Adjust navbar location

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -8,22 +8,17 @@ export const Navbar = () => {
     location.pathname === path ? 'text-white' : 'text-gray-500'
 
   return (
-    <nav className="fixed bottom-0 left-0 right-0 z-50">
-      <div className="mx-auto w-full max-w-sm">
-        <div className="flex justify-around p-2">
-          <Link
-            to="/teacher/students"
-            className={isActive('/teacher/students')}
-          >
-            <Users className="w-6 h-6" />
-          </Link>
-          <Link to="/songslist" className={isActive('/songslist')}>
-            <Music className="w-6 h-6" />
-          </Link>
-          <Link to="#" className={isActive('/teacher/settings')}>
-            <Settings className="w-6 h-6" />
-          </Link>
-        </div>
+    <nav className="fixed bottom-0 left-0 right-0 h-16 z-50 bg-neutral-900">
+      <div className="mx-auto w-full max-w-sm h-full flex items-center justify-around">
+        <Link to="/teacher/students" className={isActive('/teacher/students')}>
+          <Users className="w-6 h-6" />
+        </Link>
+        <Link to="/songslist" className={isActive('/songslist')}>
+          <Music className="w-6 h-6" />
+        </Link>
+        <Link to="#" className={isActive('/teacher/settings')}>
+          <Settings className="w-6 h-6" />
+        </Link>
       </div>
     </nav>
   )

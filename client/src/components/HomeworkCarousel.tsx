3d5da955d11c08eee59268157027ec65059af03e
Iida Häkkinen <iida.hakkinen@gmail.com>
2025-08-28 22:38:40 +0300

    Add create new homework button for teacher mode

--- client/src/components/HomeworkCarousel.tsx
+++ client/src/components/HomeworkCarousel.tsx
@@ -3,13 +3,14 @@ import { useSongsList } from '../hooks/useSongs'
 import { Link } from 'react-router-dom'
 import type { SongListItem } from '../../../shared/types'
 import SongCard from './SongCard'
-import { Plus } from 'lucide-react'
+import { Plus, Ellipsis } from 'lucide-react'
 
 type Props = {
   studentId: string
+  mode: 'teacher' | 'student'
 }
 
-export const HomeworkCarousel = ({ studentId }: Props) => {
+export const HomeworkCarousel = ({ studentId, mode }: Props) => {
   const { data: homeworkData, isPending } = useTeacherStudentHomework(studentId)
   const { data: songsData } = useSongsList()
 
@@ -34,9 +35,15 @@ export const HomeworkCarousel = ({ studentId }: Props) => {
                 className="snap-center w-full max-w-sm flex-shrink-0 rounded-lg p-4 relative"
               >
                 <div className="overflow-y-auto max-h-[calc(100dvh-220px)] pt-0 px-4 pb-4 relative">
-                  <h2 className="mb-1">
-                    {index === 0 ? 'Tehtävä' : 'Arkistoitu tehtävä'}
-                  </h2>
+                  <div className="flex items-center justify-between mb-1">
+                    <h2>{index === 0 ? 'Tehtävä' : 'Arkistoitu tehtävä'}</h2>
+                    {mode === 'teacher' && (
+                      <button type="button" className="p-1">
+                        <Ellipsis className="w-5 h-5 text-gray-200" />
+                      </button>
+                    )}
+                  </div>
+
                   <p className="text-xs text-gray-300 mb-12">
                     {new Date(hw.createdAt).toLocaleDateString()}
                   </p>
@@ -63,14 +70,16 @@ export const HomeworkCarousel = ({ studentId }: Props) => {
           <div className="w-[calc(50vw-144px)] flex-shrink-0" />
         </div>
 
-        <div className="fixed bottom-12 left-0 w-full h-20 bg-neutral-900 z-40 flex items-center justify-center">
-          <Link
-            to={`/teacher/students/${studentId}/homework/create`}
-            className="bg-white text-black rounded-full w-12 h-12 flex items-center justify-center shadow-lg"
-          >
-            <Plus size={28} strokeWidth={2.5} />
-          </Link>
-        </div>
+        {mode === 'teacher' && (
+          <div className="fixed bottom-12 left-0 w-full h-20 bg-neutral-900 z-40 flex items-center justify-center">
+            <Link
+              to={`/teacher/students/${studentId}/homework/create`}
+              className="bg-white text-black rounded-full w-12 h-12 flex items-center justify-center shadow-lg"
+            >
+              <Plus size={28} strokeWidth={2.5} />
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   )

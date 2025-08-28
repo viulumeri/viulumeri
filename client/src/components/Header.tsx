8ca933157f149159ca5daeadca94d79f671bd0b4
Iida Häkkinen <iida.hakkinen@gmail.com>
2025-08-28 22:15:23 +0300

    Add reusable header component

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+type HeaderProps = {
+  left?: React.ReactNode
+  center?: React.ReactNode
+  right?: React.ReactNode
+}
+
+export const Header = ({ left, center, right }: HeaderProps) => {
+  return (
+    <header className="sticky top-0 z-10 bg-neutral-900 px-4 pt-4 flex items-center">
+      <div className="w-10 flex justify-start">{left}</div>
+      <div className="flex-grow pl-2">{center}</div>-
+      <div className="w-10 flex justify-end">{right}</div>
+    </header>
+  )
+}

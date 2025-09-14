9721890a907e255028ff544bd5676375c7da720c
Iida Häkkinen <iida.hakkinen@gmail.com>
2025-09-14 18:06:32 +0300

    Render navbar icons based on user type

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,18 +1,36 @@
 import { Link, useLocation } from 'react-router-dom'
-import { Users, Music, Settings } from 'lucide-react'
+import { Users, Music, Settings, BookOpenText } from 'lucide-react'
+import { useSession } from '../auth-client'
+import type { AppSessionUser } from '../../../shared/types'
 
 export const Navbar = () => {
   const location = useLocation()
+  const { data: session } = useSession()
+  const userType = (session?.user as AppSessionUser | undefined)?.userType
 
   const isActive = (path: string) =>
     location.pathname === path ? 'text-white' : 'text-gray-500'
 
   return (
     <nav className="fixed bottom-0 left-0 right-0 h-16 z-50 bg-neutral-900">
-      <div className="mx-auto w-full max-w-sm h-full flex items-center justify-around">
-        <Link to="/teacher/students" className={isActive('/teacher/students')}>
-          <Users className="w-6 h-6" />
-        </Link>
+      <div className="mx-auto w-full max-w-sm h-full flex items-center justify-around pb-4">
+        {userType === 'teacher' && (
+          <Link
+            to="/teacher/students"
+            className={isActive('/teacher/students')}
+          >
+            <Users className="w-6 h-6" />
+          </Link>
+        )}
+
+        {userType === 'student' && (
+          <Link
+            to="/student/homework"
+            className={isActive('/student/homework')}
+          >
+            <BookOpenText className="w-6 h-6" />
+          </Link>
+        )}
         <Link to="/songslist" className={isActive('/songslist')}>
           <Music className="w-6 h-6" />
         </Link>

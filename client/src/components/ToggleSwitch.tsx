ded8372f4619b1d4ca49fead99343d2a68bdce36
Iida Häkkinen <iida.hakkinen@gmail.com>
2025-09-30 23:51:51 +0300

    Make entire ToggleSwitch area clickable

--- client/src/components/ToggleSwitch.tsx
+++ client/src/components/ToggleSwitch.tsx
@@ -8,25 +8,27 @@ export type ToggleSwitchProps = {
 
 export default function ToggleSwitch({ value, onChange }: ToggleSwitchProps) {
   const isSongs = value === 'songs'
+  const toggle = () => {
+    onChange(isSongs ? 'homework' : 'songs')
+  }
+
   return (
-    <div className="relative w-17 h-9 bg-gray-800 rounded-full flex items-center px-1 gap-1">
+    <button
+      type="button"
+      onClick={toggle}
+      className="relative w-17 h-9 bg-gray-800 rounded-full flex items-center px-1 gap-1"
+    >
       <div
         className={`absolute top-0 left-0 w-9 h-9 rounded-full bg-red-900 transition-all duration-300 ${
           isSongs ? 'translate-x-8' : 'translate-x-0'
         }`}
       />
-      <button
-        onClick={() => onChange('homework')}
-        className="z-10 w-9 h-9 flex items-center justify-center"
-      >
+      <div className="z-10 w-9 h-9 flex items-center justify-center">
         <BookOpen color="white" size={20} />
-      </button>
-      <button
-        onClick={() => onChange('songs')}
-        className="z-10 w-9 h-9 flex items-center justify-center"
-      >
+      </div>
+      <div className="z-10 w-9 h-9 flex items-center justify-center">
         <ListMusic color="white" size={20} />
-      </button>
-    </div>
+      </div>
+    </button>
   )
 }

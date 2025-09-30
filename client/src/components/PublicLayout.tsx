b2c413dce483bc2bfab70ee3e92aada5a24b335c
Jasse <jasse.merivirta@helsinki.fi>
2025-09-30 21:36:56 +0300

    Add max width to fix big screen look

--- client/src/components/PublicLayout.tsx
+++ client/src/components/PublicLayout.tsx
@@ -1,6 +1,6 @@
 const PublicLayout = ({ children }: { children: React.ReactNode }) => (
-  <div className="min-h-screen bg-gray-100 flex justify-center">
-    <div className="w-full max-w-sm bg-neutral-900 text-gray-100 p-4">
+  <div className="min-h-screen bg-neutral-900 text-gray-100 flex justify-center">
+    <div className="w-full max-w-4xl bg-neutral-900 text-gray-100 p-4">
       <h1>Viulumeri</h1>
       {children}
     </div>

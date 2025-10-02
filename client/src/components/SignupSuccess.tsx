7519d608c3ebb783e9490b65abab9dda5014b46e
Iida Häkkinen <iida.hakkinen@gmail.com>
2025-10-02 19:02:23 +0300

    Update SignupSuccess UI with Tailwind

--- client/src/components/SignupSuccess.tsx
+++ client/src/components/SignupSuccess.tsx
@@ -3,13 +3,18 @@ import { Link } from 'react-router-dom'
 export const SignupSuccess = () => {
   return (
     <div>
-      <h2>Käyttäjän luominen onnistui!</h2>
-      <div style={{ color: 'green', marginBottom: '16px' }}>
-        Käyttäjätunnus on luotu onnistuneesti. Vahvistuspyyntö on lähetetty sähköpostiisi.
-        Tarkista sähköpostisi ja klikkaa vahvistuslinkkiä aktivoidaksesi tilisi.
+      <h2>Käyttäjän luominen onnistui</h2>
+      <div className="p-2 mb-2 text-gray-400">
+        Lähetimme vahvistusviestin sähköpostiisi. Tarkista sähköpostisi ja
+        klikkaa vahvistuslinkkiä aktivoidaksesi tilisi.
       </div>
-      <div>
-        <Link to="/login">Siirry kirjautumaan</Link>
+      <div className="flex justify-center">
+        <Link
+          to="/login"
+          className="button-basic text-black inline-flex items-center justify-center no-underline"
+        >
+          <button>Siirry kirjautumaan</button>
+        </Link>
       </div>
     </div>
   )

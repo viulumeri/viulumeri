274d667c69b851953aa8e1477ed66837fefb1fba
Iida Häkkinen <iida.hakkinen@gmail.com>
2025-10-02 15:13:30 +0300

    Split name into two lines and truncate

--- client/src/components/StudentCard.tsx
+++ client/src/components/StudentCard.tsx
@@ -19,7 +19,10 @@ export const StudentCard = ({ id, name }: Props) => {
       className={`relative rounded-lg aspect-square w-full h-full overflow-hidden flex items-end p-4`}
       style={{ backgroundColor: bgColor }}
     >
-      <h2 className=" ">{firstName} {lastName}</h2>
+      <h3 className="grid grid-rows-2 w-full leading-tight">
+        <span className="truncate">{firstName}</span>
+        <span className="truncate">{lastName}</span>
+      </h3>
     </Link>
   )
 }

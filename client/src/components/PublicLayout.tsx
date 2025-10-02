1aaf1a24e1e23d83d7987d6a5a5a43d38b25f9b0
Iida Häkkinen <iida.hakkinen@gmail.com>
2025-10-02 21:18:50 +0300

    Prevent background image movement

--- client/src/components/PublicLayout.tsx
+++ client/src/components/PublicLayout.tsx
@@ -1,22 +1,25 @@
 const PublicLayout = ({ children }: { children: React.ReactNode }) => (
-  <div className="h-screen overflow-hidden bg-neutral-900 text-gray-100 flex flex-col justify-between">
-    <div className="relative basis-[75vh] shrink min-h-0">
-      <div
-        className="absolute inset-0 bg-cover bg-center"
-        style={{
-          backgroundImage:
-            'url("https://images.metmuseum.org/CRDImages/ad/original/ap06.1281.jpg")'
-        }}
-      />
-      <header className="relative z-10 pt-8">
-        <h1 className=" text-6xl font-semibold text-center">Viulumeri</h1>
-      </header>
-      <div className="absolute bottom-0 left-0 right-0 h-20 bg-gradient-to-b from-transparent to-neutral-900 z-10" />
-    </div>
+  <div className="relative h-screen text-gray-100">
+    <div
+      className="absolute inset-0 z-0 bg-cover bg-center"
+      style={{
+        backgroundImage:
+          'url("https://images.metmuseum.org/CRDImages/ad/original/ap06.1281.jpg")'
+      }}
+    />
 
-    <div className="w-full">
-      <div className="mx-auto w-full max-w-md px-4 pb-2">
-        <div className="p-6">{children}</div>
+    <div className="relative z-10 grid grid-rows-[minmax(0,1fr)_auto] h-full">
+      <div className="relative min-h-0">
+        <header className="relative z-10 pt-8">
+          <h1 className="text-6xl font-semibold text-center">Viulumeri</h1>
+        </header>
+        <div className="absolute bottom-0 left-0 right-0 h-20 bg-gradient-to-b from-transparent to-neutral-900 z-10" />
+      </div>
+
+      <div className="w-full bg-neutral-900">
+        <div className="mx-auto w-full px-4">
+          <div className="p-6">{children}</div>
+        </div>
       </div>
     </div>
   </div>

b459badd0690015875f7a36bc0983cb25a16be05
Iida Häkkinen <iida.hakkinen@gmail.com>
2025-10-02 21:26:19 +0300

    Set max-width

--- client/src/components/PublicLayout.tsx
+++ client/src/components/PublicLayout.tsx
@@ -1,12 +1,14 @@
 const PublicLayout = ({ children }: { children: React.ReactNode }) => (
   <div className="relative h-screen text-gray-100">
-    <div
-      className="absolute inset-0 z-0 bg-cover bg-center"
-      style={{
-        backgroundImage:
-          'url("https://images.metmuseum.org/CRDImages/ad/original/ap06.1281.jpg")'
-      }}
-    />
+    <div className="absolute inset-0 z-0 flex justify-center">
+      <div
+        className="h-full w-full max-w-4xl bg-cover bg-center"
+        style={{
+          backgroundImage:
+            'url("https://images.metmuseum.org/CRDImages/ad/original/ap06.1281.jpg")'
+        }}
+      />
+    </div>
 
     <div className="relative z-10 grid grid-rows-[minmax(0,1fr)_auto] h-full">
       <div className="relative min-h-0">
@@ -17,7 +19,7 @@ const PublicLayout = ({ children }: { children: React.ReactNode }) => (
       </div>
 
       <div className="w-full bg-neutral-900">
-        <div className="mx-auto w-full px-4">
+        <div className="mx-auto w-full max-w-md px-4">
           <div className="p-6">{children}</div>
         </div>
       </div>

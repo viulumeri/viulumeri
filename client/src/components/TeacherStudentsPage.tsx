58e7bae30ac4c8cdcb17523217eccbab63516ef7
Tiina Vilkko <vilkkotiina@gmail.com>
2026-06-13 15:55:06 +0300

    Apply PageContainer to TeacherStudentsPage

--- client/src/components/TeacherStudentsPage.tsx
+++ client/src/components/TeacherStudentsPage.tsx
@@ -1,13 +1,14 @@
 import { TeacherStudentsList } from './TeacherStudentsList'
 import { Header } from './Header'
+import { PageContainer } from './PageContainer'
 
 export const TeacherStudentsPage = () => {
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex flex-col">
       <Header left={<h1 className="ml-2">Oppilaat</h1>} />
-      <main className="flex-1 overflow-y-auto min-h-0">
+      <PageContainer>
         <TeacherStudentsList />
-      </main>
+      </PageContainer>
     </div>
   )
 }

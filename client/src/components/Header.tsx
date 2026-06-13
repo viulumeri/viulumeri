c79bafc0e9b304cc4e7f611daea0f9f8e292cb24
Tiina Vilkko <vilkkotiina@gmail.com>
2026-06-13 16:31:28 +0300

    Add sticky prop to Header component

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,11 +4,12 @@ type HeaderProps = {
   left?: React.ReactNode
   center?: React.ReactNode
   right?: React.ReactNode
+  sticky?: boolean
 }
 
-export const Header = ({ left, center, right }: HeaderProps) => {
+export const Header = ({ left, center, right, sticky = true }: HeaderProps) => {
   return (
-    <header className="sticky top-0 z-10 bg-neutral-900 px-4 pt-4 flex items-center">
+    <header className={`${sticky ? 'sticky top-0' : ''} z-10 bg-neutral-900 px-4 pt-4 flex items-center`}>
       <div className="w-10 flex justify-start">{left}</div>
       <div className="flex-grow pl-2">{center}</div>
       <div className="w-10 flex justify-end">{right}</div>

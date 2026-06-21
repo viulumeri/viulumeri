89b70d97ac24c8e30106a92913a2ec9569e135bc
A5Beef <edvintran@gmail.com>
2026-06-21 21:03:12 +0300

    Fix admin normal user view.
    When impersonating a teacher OR if admin account is a teacher account, a students homework view will not show anymore

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -28,7 +28,6 @@ import type { AppSessionUser } from '../../shared/types'
 import { NotificationProvider } from './contexts/NotificationProvider'
 import { NotificationBanner } from './components/NotificationBanner'
 import { StartupAnnouncementsPopup } from './components/StartupAnnouncementsPopup'
-import { isAdminRegularUserViewEnabled } from './utils/adminRegularUserView'
 import './index.css'
 import { InstallPromptPopup } from './components/InstallPromptPopup.tsx'
 
@@ -37,8 +36,6 @@ const App = () => {
   const userType = (session?.user as AppSessionUser | undefined)?.userType
   const role = (session?.user as AppSessionUser | undefined)?.role
   const userId = (session?.user as { id?: string } | undefined)?.id
-  const isAdminRegularUserView =
-    role === 'admin' && isAdminRegularUserViewEnabled()
 
   return (
     
@@ -176,7 +173,7 @@ const App = () => {
           )}
 
           {/* Student-only */}
-          {(userType === 'student' || isAdminRegularUserView) && (
+          {userType === 'student' && (
             <>
               <Route
                 path="/student/homework"

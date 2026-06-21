10192d4fe94457a96c30b6e3489787321f058160
Tiina Vilkko <vilkkotiina@gmail.com>
2026-06-21 17:57:37 +0300

    Add icon to TeacherStudentsPage header

--- client/src/components/TeacherStudentsPage.tsx
+++ client/src/components/TeacherStudentsPage.tsx
@@ -1,3 +1,4 @@
+import { Users } from 'lucide-react'
 import { TeacherStudentsList } from './TeacherStudentsList'
 import { Header } from './Header'
 import { PageContainer } from './PageContainer'
@@ -5,7 +6,15 @@ import { PageContainer } from './PageContainer'
 export const TeacherStudentsPage = () => {
   return (
     <div className="flex flex-col">
-      <Header left={<h1 className="ml-7">Oppilaat</h1>} sticky={false} />
+      <Header
+        left={
+          <h1 className="ml-7 flex items-center gap-3">
+            <Users className="w-8 h-8" />
+            Oppilaat
+          </h1>
+        }
+        sticky={false}
+      />
       <PageContainer>
         <TeacherStudentsList />
       </PageContainer>
